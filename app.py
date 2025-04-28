9f11b322b9f5523cce86780e58a50a6ad2737864
arteaga7 <cruz.antonio.a.e@gmail.com>
2025-04-28 17:15:11 -0600

    initial version

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
+
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+fig = px.histogram(car_data, x="odometer") # crear un histograma
+fig.show() # crear gráfico de dispersión
+
+
+
 print("Hola1")
