6613cf673dd8bd7a300dad4034cca6fde1f9d045
arteaga7 <cruz.antonio.a.e@gmail.com>
2025-04-28 23:11:00 -0600

    Final final template

--- app.py
+++ app.py
@@ -2,17 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.header('Slider')
-number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
-start_button = st.button('Ejecutar')
-
-if start_button:
-    st.write(f'Experimento con {number_of_trials} intentos en curso.')
-
-st.write('Esta aplicación aún no es funcional. En construcción.')
-
-st.header('Grafica de linea (line_chart)')
-
+#df = pd.read_csv('vehicles_us.csv')
 df = pd.DataFrame(
     {
         'x': [0,1,2,3,4,5],
@@ -21,44 +11,54 @@ df = pd.DataFrame(
     }
 )
 
-st.line_chart(data=df, x='x', y='y', x_label='None',
-              y_label='None', color=None, width=70,
+st.header('Slider')
+number_of_trials = st.slider('Number of tries?', 1, 100, 10)
+start_button = st.button('Run test')
+if start_button:
+    st.write(f'Test with {number_of_trials} tries.')
+
+st.write('Simple text as paragraph.')
+
+st.header('Line plot (line_chart)')
+st.line_chart(data=df, x='x', y='y', x_label='x_label',
+              y_label='y_label', color=None, width=70,
               height=400, use_container_width=True)
 
-st.header('Imprimir DataFrame')
+st.header('DataFrame')
 st.write(df)
-st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+st.write('Simple text as paragraph.')
+
 st.header('Histogram')
-#df = pd.read_csv('vehicles_us.csv') # leer los datos
-fig = px.histogram(df, x="y") # crear un histograma
-#fig.show() #To display in a new window
+fig_histogram = px.histogram(df, x="y")
+#fig_histogram.show() #To display in a new window
+st.plotly_chart(fig_histogram, use_container_width=True)  #To display in the same window
 
-st.plotly_chart(fig, use_container_width=True)  #To display in the same window
-
-st.header('Crea plot al dar clic')
-hist_button = st.button('Construir histograma')
-if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
-            st.write('Cches')
-            
-            # crear un histograma
-            fig2 = px.histogram(df, x="x", width=50, height=200)
-        
-            # mostrar un gráfico Plotly interactivo
+st.header('Create plot by clicking')
+hist_button = st.button('Create histogram')
+if hist_button: #if botton is pressed
+            st.write('Histogram button was pressed')
+            fig2 = px.histogram(df, x="z", width=50, height=200)
             st.plotly_chart(fig2, use_container_width=True)
 
-
+st.header('Simple botton')
+st.write('Lets create a simple botton.')
 button2 = st.button('Boton2')
-
 if button2:
-    st.write(f'Presionaste boton2')
-    
-fig_scatter = px.scatter(df, x='x',y='y') # crear un histograma
-#fig.show() #To display in a new window
+    st.write(f'Botton2 was pressed')
 
+st.header('Scatter plot') 
+fig_scatter = px.scatter(df, x='x',y='y')
 st.plotly_chart(fig_scatter, use_container_width=True)  #To display in the same window
 
-fig_bar = px.bar(df, x='x',y='y') # crear un histograma
-#fig.show() #To display in a new window
-
+st.header('Bar plot') 
+fig_bar = px.bar(df, x='x',y='y')
 st.plotly_chart(fig_bar, use_container_width=True)  #To display in the same window
+
+st.header('Checkbox') 
+build_histogram = st.checkbox('Histogram')
+if build_histogram:
+    st.write('Histogram built')
+build_scatter = st.checkbox('Scatter plot')
+if build_scatter:
+    st.write('Scatter plot built')
